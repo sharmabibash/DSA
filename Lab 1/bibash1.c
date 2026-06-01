64e5a5da493d937008a412236c34f682123ca04c
Bibash Sharma <sharmabibash77@gmail.com>
2026-06-01 15:39:45 +0545

    mmm

--- Lab 1/bibash1.c
+++ Lab 1/bibash1.c
@@ -107,15 +107,89 @@ for(i=0; infix[i]; i++) {
 
 // QN:-2 ::: B
 
-/*void reverse(char str[]){
-    int i,j=strlen(str)-1;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char op) {
+    if(op == '^')
+        return 3;
+    if(op == '*' || op == '/')
+        return 2;
+    if(op == '+' || op == '-')
+        return 1;
+    return 0;
+}
+
+void reverse(char str[]) {
+    int i, j;
     char temp;
-    
-    for(i=0,j;i<j;i++,j--){
+
+    for(i = 0, j = strlen(str) - 1; i < j; i++, j--) {
         temp = str[i];
-        str[i]=str[j];
-        str[j]=temp;
+        str[i] = str[j];
+        str[j] = temp;
     }
+}
+
+void infixToPrefix(char infix[], char prefix[]) {
+    int i, j = 0;
+    char postfix[100];
+
+    reverse(infix);
+
+    for(i = 0; infix[i] != '\0'; i++) {
+        if(infix[i] == '(')
+            infix[i] = ')';
+        else if(infix[i] == ')')
+            infix[i] = '(';
+    }
+
+  
+    top = -1;
+
+    for(i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+
+        if(isalnum(ch)) {
+            postfix[j++] = ch;
+        }
+        else if(ch == '(') {
+            push(ch);
+        }
+        else if(ch == ')') {
+            while(top != -1 && stack[top] != '(')
+                postfix[j++] = pop();
+            pop(); 
+        }
+        else {
+            while(top != -1 &&
+                  precedence(stack[top]) >= precedence(ch))
+                postfix[j++] = pop();
+
+            push(ch);
+        }
+    }
+
+    while(top != -1)
+        postfix[j++] = pop();
+
+    postfix[j] = '\0';
+
+    
+    strcpy(prefix, postfix);
+    reverse(prefix);
 }
 
 int main() {
@@ -124,21 +198,12 @@ int main() {
     printf("Enter infix expression: ");
     scanf("%s", infix);
 
-    reverse(infix);
+    infixToPrefix(infix, prefix);
 
-    for(int i=0; infix[i]; i++) {
-        if(infix[i]=='(')
-            infix[i]=')';
-        else if(infix[i]==')')
-            infix[i]='(';
-    }
-
-    printf("Apply postfix conversion on reversed expression.\n");
-    printf("Reverse obtained postfix to get prefix.\n");
+    printf("Prefix expression: %s\n", prefix);
 
     return 0;
-}*/
-
+}
 
 
 // QN : 3
@@ -187,7 +252,7 @@ int main() {
 
 
 // QN: 4
-#include <stdio.h>
+/*#include <stdio.h>
 
 char stack[100];
 int top = -1;
@@ -234,4 +299,4 @@ int main() {
         printf("Not Balanced\n");
 
     return 0;
-}
+}*/
